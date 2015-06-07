79fc522ecd5b88d61c34c6d11bc75dae88708f40
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-06-07 00:23:03 -0300

    Implementando webservice.

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ if (!fs.existsSync('config.json')) {
   process.exit(1);
 }
 
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
 
 var config = require('./config.json');
 var Twitter = require('node-twitter');
@@ -16,16 +19,41 @@ var twitterRestClient = new Twitter.RestClient(
   config.secretToken
 );
 
-twitterRestClient.statusesUpdateWithMedia({
-        'status': 'Posting a tweet w/ attached media.',
-        'media[]': 'imagen.png'
-    }, function(error, result) {
-        if (error) {
-            console.log('Error: ' + (error.code ? error.code + ' ' + error.message : error.message));
-        }
 
-        if (result) {
-            console.log(result);
-        }
+function sendMessage(message, media) {
+    twitterRestClient.statusesUpdateWithMedia({
+      'status': message,
+      'media[]': media,
+    }, function(error, result) {
+      if (error) {
+        console.log('Error: ' + (error.code ? error.code + ' ' + error.message : error.message));
+      }
+
+      if (result) {
+        console.log(result);
+      }
     }
-);
+  );
+}
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/',function(req,res){
+  res.json({info: "webservice de pilas-engine-bloques en funcionamiento."});
+});
+
+app.post('/sendMessage', function(req, res) {
+  var message = req.body.message;
+  var media = req.body.media;
+
+  console.log("message: " + message + "    media: " + media);
+
+  sendMessage(message, media);
+
+  res.json({status: "ok", message: message, media:media});
+});
+
+app.listen(3000, function(){
+  console.log("Iniciando aplicación en http://localhost:3000");
+});
