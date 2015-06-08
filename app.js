4d3ec7081f294c925057203b609d5fc6f6226cdf
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-06-07 23:55:51 -0300

    Actualizando aplicacion.

--- app.js
+++ app.js
@@ -20,16 +20,20 @@ var twitterRestClient = new Twitter.RestClient(
 );
 
 
-function sendMessage(message, media) {
+function sendMessage(message, media, callback) {
     twitterRestClient.statusesUpdateWithMedia({
       'status': message,
       'media[]': media,
     }, function(error, result) {
+
       if (error) {
-        console.log('Error: ' + (error.code ? error.code + ' ' + error.message : error.message));
+        var message = 'Error: ' + (error.code ? error.code + ' ' + error.message : error.message);
+        console.log(message);
+        callback(message, {});
       }
 
       if (result) {
+        callback(null, {url: 'https://twitter.com/pilasBloques/status/' + result.id_str});
         console.log(result);
       }
     }
@@ -48,20 +52,24 @@ app.get('/',function(req,res){
 
 app.post('/sendMessage', function(req, res) {
   var message = req.body.message;
-  console.log(req.body);
   var media = req.body.media;
   var filename = "_tmp_imagen" + 123123 + ".png";
-
 
   var base64Data = media.replace(/^data:image\/png;base64,/, "");
 
   require("fs").writeFile(filename, base64Data, 'base64', function(err) {
+
     if (err) {
       console.log(err);
       res.json({status: "error", message: err});
     } else {
-      sendMessage(message, filename);
-      res.json({status: "ok", message: message, media:media});
+      sendMessage(message, filename, function(err, data) {
+        if (err) {
+          res.json({status: "error", message: err});
+        } else {
+          res.json({status: "ok", message: message, url: data.url});
+        }
+      });
     }
   });
 
