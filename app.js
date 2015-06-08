141e7af9823a4b01435525b6e87fcffc4d051fdd
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-06-08 01:20:16 -0300

    agregando middleware para permitir posts desde pilas-bloques.

--- app.js
+++ app.js
@@ -76,6 +76,15 @@ app.post('/sendMessage', function(req, res) {
 
 });
 
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://bloques.pilas-engine.com.ar');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.listen(9914, function(){
   console.log("Iniciando aplicación en http://localhost:9914");
 });
